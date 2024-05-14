f3a2f10f1a577a2c9104930940f1b08597500193
FujiiNobutaka <fujii.nobutaka.ap@mynavi.jp>
2024-05-14 05:58:26 +0000

    "嫌いな食べ物"

---
+++ hujii/calc.py
@@ -0,0 +1,10 @@
+import sys
+args = sys.argv
+
+
+#引数を変数に代入
+a = int(args[1])
+b = int(args[2])
+c = int(args[3])
+
+print(a + b + c)
---
+++ hujii/outtext.py
@@ -0,0 +1,6 @@
+import sys
+args = sys.argv
+
+#引数を変数に代入
+food = args[1]
+print("I don't like " + '"'+ food + '"')
