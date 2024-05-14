34b3e0d94d286d2223b4d586532746f9fb8e49b7
ShigenagaHamauchi <shigenaga.hamaguchi.bl@mynavi.jp>
2024-05-14 05:52:37 +0000

    outext.pyを追加した

--- hamaguchi/calc.py
+++ hamaguchi/calc.py
@@ -3,5 +3,4 @@ import sys
 
 
 args = list(map(lambda x: int(x),sys.argv[1:]))
-
 print(args[0]+args[1]+args[2])
---
+++ hamaguchi/outext.py
@@ -0,0 +1,4 @@
+import sys
+
+for food in sys.argv[1:]:
+    print("I don't like "+'"'+food+'"',end="")
