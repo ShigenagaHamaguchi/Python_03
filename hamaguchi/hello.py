b0de70ca4f26a9266dab4073f4e8f0a94bc6b1cc
ShigenagaHamauchi <shigenaga.hamaguchi.bl@mynavi.jp>
2024-05-14 04:31:01 +0000

    引数を受け取れるmyname.pyを追加

--- hamaguchi/hello.py
+++ hamaguchi/hello.py
@@ -1,9 +1,9 @@
 import sys
 
 args = sys.argv
-
+print(sys.path)
 if "-h" in args:
     print("hello world")
-elif "-i" in args:
-    hello = input()
-    print(hello)
+elif "-n" in args:
+    name = input()
+    print("Hello",name)
