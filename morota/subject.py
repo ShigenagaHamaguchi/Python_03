e37518ed7bb30a1b2f34462cee93ef4125a02f4e
WarabiMorota <morota.warabi.jj@mynavi.jp>
2024-05-14 08:32:02 +0000

    編集追加

--- morota/subject.py
+++ morota/subject.py
@@ -7,7 +7,7 @@ japanese = int(args[2])
 english = int(args[3])
 
 
-if (math >= 50) and (japanese >= 50) and (english >= 50):              #1科目も50点未満がない
+if (math >= 50) and (japanese >= 50) and (english >= 50):           #1科目も50点未満がない
     if (math >= 70) and (japanese >= 70) and (english >= 70):       #3教科とも70点以上
         print("合格", end="")
     elif math + japanese + english >= 220:                          #合計点数が220点以上
