6d3f7729944060a2d69cf75be569b3910cf0d5d3
ShigenagaHamauchi <shigenaga.hamaguchi.bl@mynavi.jp>
2024-05-14 07:20:44 +0000

    コメント追加

--- hamaguchi/evenadd.py
+++ hamaguchi/evenadd.py
@@ -1,7 +1,10 @@
 import sys
-p = lambda x: print(x,end="")
+"""
+引数を取得し、int型に変換する。
+その後、偶奇判定を行う。
+"""
 nums = list(map(lambda x:int(x),sys.argv[1:]))
-
+p = lambda x: print(x,end="")
 for num in nums:
     if num % 2 == 0:p("偶数")
     else:p("奇数")
