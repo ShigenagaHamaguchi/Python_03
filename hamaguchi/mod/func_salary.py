979504ed2a28e349fa4aa63b0dd88d67ba9f3b8e
ShigenagaHamauchi <shigenaga.hamaguchi.bl@mynavi.jp>
2024-05-17 06:34:02 +0000

    コメントを追記

--- hamaguchi/mod/func_salary.py
+++ hamaguchi/mod/func_salary.py
@@ -1,17 +1,17 @@
 """
-
-
+給与から税額と支払額を算出するプログラム
 """
 
-B = 1000000
+B = 1000000 # ボーダー
 
 from decimal import Decimal,ROUND_HALF_UP
 
 def payamount_calculation(salary):
-    if (salary < B) :
+    if (salary < B) : # ボーダーを下回っていた場合の税額
         tax = salary * 0.1
-    else :
+    else : # ボーダーを上回っていた場合の税額
         tax  = (salary-B)*0.2 + B*0.1
+    
     tax = Decimal(str(tax)).quantize(Decimal("0"),rounding=ROUND_HALF_UP)
     payamount = salary - tax
     return payamount,tax
