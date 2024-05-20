b632f5745a1be284e087372c37ae06f0756ad122
ShigenagaHamauchi <shigenaga.hamaguchi.bl@mynavi.jp>
2024-05-20 01:45:48 +0000

    コメントの追加

--- hamaguchi/makerqr.py
+++ hamaguchi/makerqr.py
@@ -2,8 +2,9 @@ import qrcode
 import os
 import sys
 
-
-
+# qrコードの作成
 img = qrcode.make(sys.argv[1])
+# qrコードの保存パス
 path = os.path.join(".","Seminar","python","files",sys.argv[2])
+# qrコードの保存
 img.save(path)
